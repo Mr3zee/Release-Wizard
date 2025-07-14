c2cdd12b45cf83bcd5aa4711f2a0baacada82548
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-07-14 22:46:17 +0200

    init commit

---
+++ shared/src/jvmMain/kotlin/io/github/mr3zee/rwizard/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package io.github.mr3zee.rwizard
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
