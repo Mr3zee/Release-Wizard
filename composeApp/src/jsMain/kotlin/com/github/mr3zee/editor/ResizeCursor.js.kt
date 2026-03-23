adaa43e721bb34d228e5cd4698df5a6ae9596cd2
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2026-03-23 21:54:23 +0100

    Fix pointers in web

--- composeApp/src/jsMain/kotlin/com/github/mr3zee/editor/ResizeCursor.js.kt
+++ composeApp/src/jsMain/kotlin/com/github/mr3zee/editor/ResizeCursor.js.kt
@@ -1,14 +1,17 @@
 package com.github.mr3zee.editor
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.fromKeyword
 
+@OptIn(ExperimentalComposeUiApi::class)
 internal actual fun resizeEdgeCursor(edge: ResizeEdge): PointerIcon = when (edge) {
-    ResizeEdge.Top -> PointerIcon("n-resize")
-    ResizeEdge.Bottom -> PointerIcon("s-resize")
-    ResizeEdge.Left -> PointerIcon("w-resize")
-    ResizeEdge.Right -> PointerIcon("e-resize")
-    ResizeEdge.TopLeft -> PointerIcon("nw-resize")
-    ResizeEdge.TopRight -> PointerIcon("ne-resize")
-    ResizeEdge.BottomLeft -> PointerIcon("sw-resize")
-    ResizeEdge.BottomRight -> PointerIcon("se-resize")
+    ResizeEdge.Top -> PointerIcon.fromKeyword("n-resize")
+    ResizeEdge.Bottom -> PointerIcon.fromKeyword("s-resize")
+    ResizeEdge.Left -> PointerIcon.fromKeyword("w-resize")
+    ResizeEdge.Right -> PointerIcon.fromKeyword("e-resize")
+    ResizeEdge.TopLeft -> PointerIcon.fromKeyword("nw-resize")
+    ResizeEdge.TopRight -> PointerIcon.fromKeyword("ne-resize")
+    ResizeEdge.BottomLeft -> PointerIcon.fromKeyword("sw-resize")
+    ResizeEdge.BottomRight -> PointerIcon.fromKeyword("se-resize")
 }
